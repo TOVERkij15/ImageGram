2f1c189898804176345d6a74b3a1f8aca2e1f30e
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-06-17 09:42:57 -0400

    2nd commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,7 @@ Parse.initialize("MaYyxQU280uNPxkOE52SHC2FyIBPulTOHcXFPtTV", "Vq6YQDoSdt49lG2tyB
 var ImageGram = Parse.Object.extend({
 	className: "ImageGram",
 });
+
 //Creates a new instance of that class.
 var imageGram = new ImageGram();
 //saves url and caption inputs
@@ -19,19 +20,59 @@ $('.button').click(function(){
 			}
 		});
 	})
+
 //refresh object
-imageGram.fetch({
-	success:function(imageGram){
+		imageGram.fetch({
+			success: function(imageGram){
+		
 		console.log('refresh bitch!')
+			},
+			error: function(object,error) {
+
+		}
+});
+//collection
+var ImageGramCollection = Parse.Collection.extend({
+	model:ImageGram
+});
+
+var collection = new ImageGramCollection();
+
+		collection.fetch({
+			success: function(collection){
+		collection.each(function(objects){
+			console.log('objects');
+		});
 	},
-	error:function(imageGram,error){
+			error: function(collection,error){
 
 	}
-});
+	});
+
+//view
+/*var ImageGramView = Parse.View.extend({
+	className: 'ImageGram',
+
+	template: _.template($('.image-template').text()),
+
+
+	initialize: function(){
+	$('.image-container').append(this.el);
+	this.render();
+},
+
+	render:function(ImageGram){
+	if(this.model.attributes.hasOwnProperty('url')){
+		var renderedTemplate = this.template(this.model.attributes);
+		this.$el.html(renderedTemplate);
+	}
+},
+});*/
+
 
 //To create a new post with a single comment 
 //Types
-var Post = Parse.Object.extend("Post");
+/*var Post = Parse.Object.extend("Post");
 var Comment = Parse.Object.extend("Comment");
 
 //Creating Post
@@ -49,11 +90,23 @@ imageComment.set("parent",imagePost);
 //Saves everything
 imageComment.save();
 
+var Imagepost = imageComment.get("parent");
+Imagepost.fetch({
+  success: function(Imagepost) {
+    var title = Imagepost.get("title");
+  }
+});*/
 
 
+var ImageGram = Parse.Object.extend("ImageGram");
+var query = new Parse.Query(ImageGram);
+query.get("YdKMxS47OC",{
+	success: function(objects) {
+	},
+	error: function(objects, error) {
 
- 
-
+	}
+});
 
  
 
