f2295c7d60e429b687f69ce22ede7fdb89fc83f2
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-06-20 22:59:45 -0400

    updated1

--- app/scripts/collections.js
+++ app/scripts/collections.js
@@ -11,19 +11,3 @@ var ImageGramCollection = Parse.Collection.extend({
 
 
 
-//collection
-/*var ImageGramCollection = Parse.Collection.extend({
-	model:ImageGram
-});
-
- var collection = new ImageGramCollection();
-	collection.fetch({
-  success: function(imageGram) {
-    collection.each(function(object) {
-      console.log(object);
-    });
-  },
-  error: function(imageGram, error) {
-    
-  }
-});*/
